eb44847d25c521df2fb5c99a17e0ce4abf329deb
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-31 18:14:51 +0530

    [Raj] Added:- The message in the main program

---
+++ GenericsProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the array print program using generic class and method ");
+            Console.ReadLine();
+        }
+    }
+}
